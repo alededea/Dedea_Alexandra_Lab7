e9cd2fe2b5b5a12bcd2bb29ab1f3b7c6571acb1b
Alexandra Dedea <dedea.alexandra19@outlook.com>
2023-11-24 13:47:33 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Dedea_Alexandra_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Dedea_Alexandra_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
